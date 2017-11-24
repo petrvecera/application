765c3eba43c16992f085dc8c0af0ad1996b5cd69
Petr Vecera <petr.vecera@profiq.com>
2017-11-24 14:40:45 +0100

    Implement the export view

--- public/js/global.js
+++ public/js/global.js
@@ -24,6 +24,26 @@ $(document).ready(function(){
       $('input.book-leave-to-input').datepicker('setDate', $('input.book-leave-from-input').datepicker('getFormattedDate'));
     }
   });
+
+  // The date picker form for exporting the data
+  $('input.export-from-input').on('change', function(e){
+    e.stopPropagation();
+
+    var from_date = $('input.export-from-input').datepicker('getDate');
+
+    if ( ! from_date ) {
+      // no new value for FROM part, do nothing
+      console.log('No from date');
+      return;
+    }
+
+    var to_date = $('input.export-to-input').datepicker('getDate');
+
+    if ( ! to_date || ( to_date && to_date.getTime() < from_date.getTime() )) {
+      $('input.export-to-input').datepicker('setDate', $('input.export-from-input').datepicker('getFormattedDate'));
+    }
+  });
+
 });
 
 
